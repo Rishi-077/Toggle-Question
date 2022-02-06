07d7059392f8062c40eb3beeda0883259e611ced
Rishi-077 <rishikesan070@gmail.com>
2022-02-06 22:48:06 +0530

    added some style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
-function App() {
+import data from './components/data';
+import Question from './components/Question';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card} from 'react-bootstrap';
+function App ()
+{
+  const [ questions, setQuestion ] = useState( data );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <Card className="card">
+          <h3>questions and answers about logins</h3>
+          {questions.map( question =>
+          { return <Question key={question.id} {...question} />;
+          }
+          )}
+        </Card>     
+      </div>     
+    </main>
   );
 }
 
